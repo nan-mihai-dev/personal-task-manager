da93261d01fe2c5df34f2b26ecb99235dff5a81d
nan-mihai-dev <nan.mihai.dev@gmail.com>
2024-11-14 13:46:40 +0200

    Styling, add create task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,16 +3,10 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+    @task = Task.new
   end
 
   def show
-  end
-
-  def new
-    @task = Task.new(task_params)
-    if @task.save
-      redirect_to tasks_path, notice: "Task was successfully created."
-    end
   end
 
   def edit
@@ -20,37 +14,24 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_back_or_to tasks_path, notice: "Task was successfully created."
+    else
+      redirect_back_or_to tasks_path, alert: "The task could not be created."
     end
   end
 
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_back_or_to tasks_path, notice: "Task was successfully created."
+    else
+      redirect_back_or_to tasks_path, alert: "The task could not be created."
     end
   end
 
   def destroy
     @task.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to tasks_path, status: :see_other, notice: "Task was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_back_or_to tasks_path, notice: "Task was successfully destroyed."
   end
 
   private
