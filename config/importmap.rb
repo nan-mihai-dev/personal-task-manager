3780f02cdfe6ac26420c9f4518ee07ef8d1f3ef5
nan-mihai-dev <nan.mihai.dev@gmail.com>
2024-11-15 15:31:58 +0200

    Add jquery the correct way

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,5 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"
+pin "jquery", to: "jquery.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
